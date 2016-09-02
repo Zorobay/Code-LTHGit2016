965f8708ef435e3a8ab33d4615e93f84f7c54617
Sebastian Hegardt <se.hegardt@gmail.com>
2016-09-02 13:55:03 +0200

    Added functionality to read from file

--- main.scala
+++ main.scala
@@ -1,5 +1,20 @@
 object start{
-	def Main(args: Array[String]){
-		println("hej code@LTH");
+	def main(args: Array[String]): Unit = {
+		val encryptedMessage = reader.readMessage
+		println(encryptedMessage)
+	}
+}
+
+object reader{
+	def readMessage(): String = {
+		import scala.io.Source
+		var msg = "???"
+		try{
+			msg = Source.fromFile("secretmessage.txt").getLines.mkString
+		}catch{
+			case ex: Exception => println("Cant find file secretmessage.txt, did you move it?")
+		}
+		
+		msg
 	}
 }
