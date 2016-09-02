6e162d7aa8e65195d6ebd227d60f88dc5ddba56d
Sebastian Hegardt <se.hegardt@gmail.com>
2016-09-02 14:22:22 +0200

    added function to print flashy greeting

--- main.scala
+++ main.scala
@@ -1,8 +1,23 @@
 object start{
 	def main(args: Array[String]): Unit = {
 		val encryptedMessage = reader.readMessage
-		println(encryptedMessage)
+
+    printGreeting(encryptedMessage)
 	}
+
+  def printGreeting(encMsg: String): Unit ={
+    val msg = "We have recieved a top secret message..."
+    println(msg)
+    println("-" * msg.length + "\n" * 2)
+    print("reading.")
+
+    for(i <- 1 to 5){
+      print(".")
+      Thread.sleep(1000)
+    }
+
+    print("\nIncoming message: " + encMsg)
+  }
 }
 
 object reader{
